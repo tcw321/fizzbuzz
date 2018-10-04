99cb3f8eae413b0abdbff675f1a15be532513d83
Timothy C. Wright <tcw321@gmail.com>
2018-10-03 20:36:15 -0400

    initial commit

---
+++ tests/com/frc3322/MainTest.java
@@ -0,0 +1,13 @@
+package com.frc3322;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void is_everything_configured() {
+        assertTrue(true);
+    }
+
+}
